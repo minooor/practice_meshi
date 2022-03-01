1c58553a3383fa77ffca190549d76c932280b06c
EC2 Default User <ec2-user@ip-172-31-13-209.ec2.internal>
2022-03-01 08:44:25 +0000

    [Add] コメント機能

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,9 +1,9 @@
 class PostImagesController < ApplicationController
-  
+
   def new
     @post_image = PostImage.new
   end
-  
+
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
@@ -17,19 +17,20 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
-  
+
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
     redirect_to post_images_path
   end
-  
+
   # 投稿データのストロングパラメータ
   private
 
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
+
 end
